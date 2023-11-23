8297271dae8d5e29bae20fe17d831d568eaa6881
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-11-23 16:01:36 +0100

    api url and keys added

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "fbc9ebbe8emsh5bb9bfc5570231dp1add18jsnc9835428896f",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "09d9afff35b2db53448189639f957b90";
